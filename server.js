3d4fde5eb53e0cebac3a58e70013c70d94a7293c
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2026-08-19 19:24:32 -0700

    connecting code to mongo db

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const  express = require('express');
+
+const mongodb = require('./data/database');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err); 
+
+  }
+  else {
+    app.listen(PORT, () => {console.log(`Database is listening and node running on port ${PORT}`)});
+  }
+});
 
 app.listen (PORT, () => {console.log(`Server is running on port ${PORT}`)});
