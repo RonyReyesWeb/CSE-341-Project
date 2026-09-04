2fe61e917bc826fe6b55472579a0c0432c3804c1
Rony Reyes <rony@caidci.com>
2026-09-04 13:16:11 -0700

    updating the server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const  express = require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
 const app = express();
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    'Access-Control-Allow-Methods',
+    'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
   );
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
@@ -20,12 +20,10 @@ app.use('/', require('./routes'));
 
 mongodb.initDb((err) => {
   if (err) {
-    console.log(err); 
-
-  }
-  else {
-    app.listen(PORT, () => {console.log(`Database is listening and node running on port ${PORT}`)});
+    console.log(err);
+  } else {
+    app.listen(PORT, () => {
+      console.log(`Database is connected, server running on port ${PORT}`);
+    });
   }
 });
-
-app.listen (PORT, () => {console.log(`Server is running on port ${PORT}`)});
