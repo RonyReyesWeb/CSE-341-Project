0c14bae9e1033d7fb3e68fab80aed468670db0ba
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2026-09-05 18:33:18 -0700

    modifying the swgger directing it to localhost

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'Users Api',
     description: 'Users APi',
   },
-  host: 'localhost:3000',
-  schemes: ['http', 'https'],
+  host: 'project1-hmul.onrender.com',
+  schemes: ['https'],
 };
 
 const outputFile = './swagger.json';
